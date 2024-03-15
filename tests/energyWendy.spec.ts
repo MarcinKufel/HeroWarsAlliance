60100faf074b5c11271e0d3cc99cac6b7bdaf7a3
Marcin Kufel <marcinkufel84@gmail.com>
2024-03-15 18:36:14 +0100

    fix: fix broken test

--- tests/energyWendy.spec.ts
+++ tests/energyWendy.spec.ts
@@ -2,9 +2,10 @@ import { expect, test } from '@playwright/test';
 
 test('home page title', async ({ page }) => {
     await page.goto('https://wendy-shop.nexters.com/')
-    await expect(page).toHaveURL('https://wendy-shop.nexters.com/')
-
+    await expect(page).toHaveURL('https://wendy-shop.nexters.com/');
+    await page.waitForLoadState()
     await page.getByRole('button', { name: 'Collect' }).click();
     await page.getByRole('textbox', { name: '0000000000' }).fill("610700832");
-    await page.getByRole('button', { name: 'Collect' }).first().click();
+    await page.waitForTimeout(3000)
+    await page.getByRole('button', { name: 'Collect' }).first().click()
 })
