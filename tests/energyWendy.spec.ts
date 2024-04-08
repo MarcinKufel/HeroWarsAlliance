fc0d2197593bdd8040192b2e19cf0a51ecb28ee9
Marcin Kufel <marcin.k@bitnoi.se>
2024-04-08 12:14:32 +0200

    fix: changed ID

--- tests/energyWendy.spec.ts
+++ tests/energyWendy.spec.ts
@@ -5,7 +5,7 @@ test('home page title', async ({ page }) => {
     await expect(page).toHaveURL('https://wendy-shop.nexters.com/');
     await page.waitForLoadState()
     await page.getByRole('button', { name: 'Collect' }).click();
-    await page.getByRole('textbox', { name: '0000000000' }).fill("610700832");
+    await page.getByRole('textbox', { name: '0000000000' }).fill("564051856");
     await page.waitForTimeout(6000)
     await page.getByRole('button', { name: 'Collect' }).first().click()
     await page.waitForTimeout(6000)
